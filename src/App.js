c557ae0532fd031f15808dc2a4fb5fa2e9b209a4
yinghui.zhao <sishenlail12@gmail.com>
2023-02-15 11:01:00 +0800

    fix: 手动配置eslint解决import报错问题

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
+// 测试朋友问的一个问题
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [useSource, setSource] = useState("http://192.168.0.1/av.mp4")
+  useEffect(() => {})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          new project
-        </a>
+        <video onError={(e) => {if(e.nativeEvent.type === 'error'){ setSource('http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4') }}} width={400} height={400} className='videoPlayer' controls src={useSource}>
+        </video>
       </header>
     </div>
   );
